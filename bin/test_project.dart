bc06d4e8afdf8c8a9d5494d8863cb4c3e42a9b47
Munjid <mungednour@gmail.com>
2025-02-06 15:08:37 +0300

    refactor functions

--- bin/test_project.dart
+++ bin/test_project.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'box_save.dart';
 import 'task.dart';
 import 'todos.dart';
@@ -11,65 +12,88 @@ void main() async {
   Todos todos = Todos(byKey.cast<Task>());
 
   print('Welcome in Todos App!');
-
   bool isWorking = true;
-
   while (isWorking) {
     List<String> sprint = _printMessages();
     stdout.write('Select from 1 to ${sprint.length - 1}:\n');
-
     String? option = stdin.readLineSync();
-
     print('\n');
     switch (option) {
       case '1':
-        stdout.write('Enter a task title: ');
-        String? taskTitle = stdin.readLineSync();
-        if (taskTitle != null) {
-          todos.addTask(taskTitle);
-        }
+        _addTasks(todos);
         break;
       case '2':
-        stdout.write('Enter a task Id: ');
-        String? taskId = stdin.readLineSync();
-        todos.tryToRemoveTask(taskId);
+        _removeTasks(todos);
         break;
       case '3':
-        print('The new tasks');
-        todos.viewTodos();
-        print('*****************');
+        _viewTasks(todos);
         break;
       case '4':
-        stdout.write('Enter a task ID: ');
-        String? taskIdString = stdin.readLineSync();
-        int taskId = int.tryParse(taskIdString ?? '') ?? 0;
-        if (taskId != 0) {
-          todos.markTaskAsCompleted(taskId);
-        }
+        _markCompleted(todos);
         break;
       case '5':
-        stdout.write('Enter a task ID: ');
-        String? taskIdString = stdin.readLineSync();
-        int taskId = int.tryParse(taskIdString ?? '') ?? 0;
-        stdout.write('Enter a new title: ');
-        String? newTitle = stdin.readLineSync();
-        if (taskId != 0 && newTitle != null) {
-          todos.updateTask(taskId, newTitle);
-        }
+        _updateTasks(todos);
         break;
       case '6':
-        // Todo: Make all needed action work with save action. (Ok?)
-        await storage.save(listKeyAtStorage, todos.myList);
+        await _deleteAllTasks(storage, listKeyAtStorage, todos);
         break;
       case '7':
-        await storage.deleteByKey(listKeyAtStorage);
-        todos.clearTask();
-        print('Deleted :)');
-        break;
-      case '8':
+        await storage.save(listKeyAtStorage, todos.myList);
         isWorking = false;
         break;
     }
+  }
+}
+
+Future<void> _deleteAllTasks(
+    StorageBox storage, String listKeyAtStorage, Todos todos) async {
+  await storage.deleteByKey(listKeyAtStorage);
+  todos.clearTask();
+  print('Deleted :)');
+  print('');
+}
+
+void _updateTasks(Todos todos) {
+  stdout.write('Enter a task ID: ');
+  String? taskIdString = stdin.readLineSync();
+  int taskId = int.tryParse(taskIdString ?? '') ?? 0;
+  stdout.write('Enter a new title: ');
+  String? newTitle = stdin.readLineSync();
+  if (taskId != 0 && newTitle != null) {
+    todos.updateTask(taskId, newTitle);
+  }
+}
+
+void _markCompleted(Todos todos) {
+  stdout.write('Enter a task ID: ');
+  String? taskIdString = stdin.readLineSync();
+  int taskId = int.tryParse(taskIdString ?? '') ?? 0;
+  if (taskId != 0) {
+    todos.markTaskAsCompleted(taskId);
+  }
+}
+
+void _viewTasks(Todos todos) {
+  print('The new tasks');
+  todos.viewTodos();
+  print('');
+}
+
+void _removeTasks(Todos todos) {
+  stdout.write('Enter a task Id: ');
+  String? taskId = stdin.readLineSync();
+  todos.tryToRemoveTask(taskId);
+  //Rearrange id List
+  for (int i = 0; i < todos.myList.length; i++) {
+    todos.myList[i].id = i + 1;
+  }
+}
+
+void _addTasks(Todos todos) {
+  stdout.write('Enter a task title: ');
+  String? taskTitle = stdin.readLineSync();
+  if (taskTitle != null) {
+    todos.addTask(taskTitle);
   }
 }
 
@@ -81,11 +105,10 @@ List<String> _printMessages() {
     '3. View all tasks',
     '4. Mark a task as completed',
     '5. Update task title',
-    '6. Save data',
-    '7. Delete data',
-    '8. Exit'
+    '6. Delete data',
+    '7. Exit'
   ];
-  
+
   for (String msg in sprint) {
     print(msg);
   }
